de33233c5f93d66fd1baf5b6a0c9396d859dc0bd
Jakub Wolanin <wolanin.kontakt@gmail.com>
2024-05-26 14:09:12 +0200

    Add functionality to mark books as favorites on double-click

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,50 @@
 {
-    'use strict';
+  'use strict';
 
-    const select = {
-        templateOf: {
-            book: '#template-book',
-        },
-        containerOf: {
-            bookList: '.books-list',
-        },
-    };
+  const select = {
+    templateOf: {
+      book: '#template-book',
+    },
+    containerOf: {
+      bookList: '.books-list',
+    },
+  };
 
-    const templates = {
-        book: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
-    };
+  const templates = {
+    book: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
+  };
 
-    function render() {
-        const booksListContainer = document.querySelector(select.containerOf.bookList);
+  const favoriteBooks = [];
 
-        for (let book of dataSource.books) {
+  function render() {
+    const booksListContainer = document.querySelector(select.containerOf.bookList);
 
-            const generatedHTML = templates.book(book);
-            const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+    for (let book of dataSource.books) {
 
-            booksListContainer.appendChild(generatedDOM);
-        }
+      const generatedHTML = templates.book(book);
+      const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+
+      booksListContainer.appendChild(generatedDOM);
     }
-    render();
+  }
+
+  function initActions() {
+
+    const bookImages = document.querySelectorAll('.book__image');
+
+    for (let bookImage of bookImages) {
+      bookImage.addEventListener('dblclick', function (event) {
+        event.preventDefault();
+
+        bookImage.classList.add('favorite');
+
+        const bookId = bookImage.getAttribute('data-id');
+
+        favoriteBooks.push(bookId);
+      });
+    }
+  }
+
+  render();
+  initActions();
 }
