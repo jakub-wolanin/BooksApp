de2652c2288986f37a179f8feaa40e27e96da535
Jakub Wolanin <wolanin.kontakt@gmail.com>
2024-06-01 14:47:18 +0200

    Add function determineRatingBgc

--- src/js/script.js
+++ src/js/script.js
@@ -26,6 +26,16 @@
 
     for (let book of dataSource.books) {
 
+      const ratingBgc = determineRatingBgc(book.rating);
+      const ratingWidth = book.rating * 10;
+
+      book.ratingBgc = ratingBgc;
+      book.ratingWidth = ratingWidth;
+
+      console.log('Book:', book);
+      console.log('Rating Width:', ratingWidth);
+      console.log('Rating Bgc:', ratingBgc);
+
       const generatedHTML = templates.book(book);
       const generatedDOM = utils.createDOMFromHTML(generatedHTML);
 
@@ -99,6 +109,18 @@
       } else {
         bookElement.classList.remove('hidden');
       }
+    }
+  }
+
+  function determineRatingBgc(rating) {
+    if (rating < 6) {
+      return 'linear-gradient(to bottom, #fefcea 0%, #f1da36 100%)';
+    } else if (rating > 6 && rating <= 8) {
+      return 'linear-gradient(to bottom, #b4df5b 0%, #b4df5b 100%)';
+    } else if (rating > 8 && rating <= 9) {
+      return 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
+    } else {
+      return 'linear-gradient(to bottom, #ff0084 0%, #ff0084 100%)';
     }
   }
 
